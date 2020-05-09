15d30c36daee7fae78737b9c95c909b2dfcd3b08
James B Schoen <edensongjbs@gmail.com>
2020-05-09 20:37:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,5 +17,6 @@ end
 
 def get_english_meaning(file_path, emoticon)
   # code goes here
-  (load_library(file_path).find{|key, value| value[:japanese]==emoticon})[0]
+  full_hash_as_array=(load_library(file_path).find{|key, value| value[:japanese]==emoticon})
+  full_hash_as_array[0]
 end
