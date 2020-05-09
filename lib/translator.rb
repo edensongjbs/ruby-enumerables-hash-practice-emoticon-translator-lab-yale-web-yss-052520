5d48885d97182b196eebe3a9b56bb184d2ce76dc
James B Schoen <edensongjbs@gmail.com>
2020-05-09 20:05:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,10 +3,12 @@ require "yaml"
 
 def load_library(file_path)
   # code goes here
-  p file_path
+  #p file_path
   emo_library=YAML.load_file(file_path)
-  p emo_library
+  #p emo_library
   emo_library.reduce({}) do |memo, (key, value_array)|
+    p key
+    p value_array
     memo[key][:english]=value_array[0]
     memo[key][:japanese]=value_array[1]
     memo
