6e4116d01d0d22db5277e96a8fc02933a32475af
James B Schoen <edensongjbs@gmail.com>
2020-05-09 19:55:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,11 @@
 # require modules here
+require "yaml"
 
 def load_library
   # code goes here
+  emo_library=YAML.load_file('lib/emoticons.yml')
+  p emo_library
+  emo_library.collect do 
 end
 
 def get_japanese_emoticon
@@ -11,3 +15,5 @@ end
 def get_english_meaning
   # code goes here
 end
+
+load_library
