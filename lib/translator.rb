6b8cfb7f1d017ed0169b7d0253f372d74da6d257
James B Schoen <edensongjbs@gmail.com>
2020-05-09 20:00:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,11 @@ def load_library
   # code goes here
   emo_library=YAML.load_file('lib/emoticons.yml')
   p emo_library
-  #emo_library.reduce do { 
+  emo_library.reduce({}) do |memo, (key, value_array)|
+    memo[key][:english]=value_array[0]
+    memo[key][:japanese]=value_array[1]
+    memo
+  end
 end
 
 def get_japanese_emoticon
