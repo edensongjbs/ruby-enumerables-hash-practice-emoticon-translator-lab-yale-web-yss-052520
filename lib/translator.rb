e177cbe00bc79fa52f9abf9ee595f6774cfc40fb
James B Schoen <edensongjbs@gmail.com>
2020-05-09 20:08:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,5 +1,6 @@
 # require modules here
 require "yaml"
+require 'pry'
 
 def load_library(file_path)
   # code goes here
@@ -9,6 +10,7 @@ def load_library(file_path)
   emo_library.reduce({}) do |memo, (key, value_array)|
     p key
     p value_array
+    binding.pry
     memo[key][:english]=value_array[0]
     memo[key][:japanese]=value_array[1]
     memo
